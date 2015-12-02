d84cd4aac91087c7e0642e6fa84d07f024f47049
oadam11 <ovadia.adam@gmail.com>
2015-12-02 18:45:17 -0500

    fix compile issues
    Change-Id: I44f3a8efd788074b10403c0c19919808a051b089

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -190,18 +190,10 @@ void kgsl_pwrctrl_pwrlevel_change(struct kgsl_device *device,
 		if (pwr->ebi1_clk)
 			clk_set_rate(pwr->ebi1_clk, pwrlevel->bus_freq);
 
-	if (test_bit(KGSL_PWRFLAGS_CLK_ON, &pwr->power_flags) ||
-		(device->state == KGSL_STATE_NAP)) {
 	pwrlevel = &pwr->pwrlevels[pwr->active_pwrlevel];
 	clk_set_rate(pwr->grp_clks[0], pwrlevel->gpu_freq);
 	trace_kgsl_pwrlevel(device, pwr->active_pwrlevel,
 			pwrlevel->gpu_freq);
-
-#ifdef CONFIG_CPU_FREQ_GOV_SLIM
-        graphics_boost = pwr->active_pwrlevel;
-#endif
-}
-EXPORT_SYMBOL(kgsl_pwrctrl_pwrlevel_change);
 
 		/*
 		 * On some platforms, instability is caused on
@@ -223,12 +215,22 @@ EXPORT_SYMBOL(kgsl_pwrctrl_pwrlevel_change);
 			clk_set_rate(pwr->grp_clks[0],
 				pwr->pwrlevels[level].gpu_freq);
 		}
-	}
+	
 
 
 	trace_kgsl_pwrlevel(device, pwr->active_pwrlevel, pwrlevel->gpu_freq);
-}
 
+
+
+
+
+#ifdef CONFIG_CPU_FREQ_GOV_SLIM
+        graphics_boost = pwr->active_pwrlevel;
+#endif
+}
+EXPORT_SYMBOL(kgsl_pwrctrl_pwrlevel_change);
+
+		
 EXPORT_SYMBOL(kgsl_pwrctrl_pwrlevel_change);
 
 static int kgsl_pwrctrl_thermal_pwrlevel_store(struct device *dev,
